95d9f969ce010867b2aebb928804f7eed4adceb4
samedamci <samedamci@disroot.org>
2020-07-20 16:46:20 +0200

    Add support for dynamic refreshing of screen

--- Emulator/emulator.py
+++ Emulator/emulator.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import Tk, Canvas, Frame, BOTH, Button
 from Emulator.bytes import BYTES
+from Emulator.sectors import LINES
+
+app = Tk()
 
 
 class ctx(object):
+    """
+    Emulator context.
+    """
+
     title = "ZeroSeg Simulator"
     off_color = "#bebebe"
     on_color = "#fc0505"
+    update_delay = 10  # ms
+    method = []
 
 
 class Display(Frame):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+    """
+    Main emulator class.
+    """
 
-    def initUI(self):
-        self.master.title(ctx.title)
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
         self.pack(fill=BOTH, expand=1)
         self.canvas = Canvas(self)
+        self.init_lines()
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.init_buttons()
 
-        # Draw horizontal lines.
+        self.update_handler()
+
+    def init_lines(self):
+        """
+        Draw gray (not activated) lines.
+        """
+        # Horizontal.
         for i in range(8):
             for j in range(3):
                 self.canvas.create_line(
@@ -31,7 +50,7 @@ class Display(Frame):
                     fill=ctx.off_color,
                 )
 
-        # Draw vertical lines.
+        # Vertical.
         for i in range(16):
             self.canvas.create_line(
                 10 + (i * 30), 25, 10 + (i * 30), 61, fill=ctx.off_color
@@ -40,122 +59,78 @@ class Display(Frame):
                 10 + (i * 30), 61, 10 + (i * 30), 96, fill=ctx.off_color
             )
 
-        self.canvas.pack(fill=BOTH, expand=1)
+    def init_buttons(self):
+        """
+        Initialize buttons (not working yet).
+        """
+        self.button_left = Button(self)
+        self.button_left.pack(fill=BOTH)
+        self.button_right = Button(self)
+        self.button_right.pack(fill=BOTH)
+
+    def update_handler(self):
+        """
+        Call first method from queue (`ctx.method`) for update screen purpose.
+        """
+        try:
+            eval(f"self.{ctx.method[0]}")
+            del ctx.method[0]
+        except IndexError:
+            pass
+        self.after(ctx.update_delay, self.update_handler)
 
     def draw_symbol(self, byte: int, position: int):
-        # Coordinates of lines on each sector.
-        # Each external `list` is a one display sector and each internal list is
-        # one line in this sector sorted in schematic way by index in list.
-        self.lines = [
-            [None],
-            [
-                [430, 25, 460, 25],
-                [460, 25, 460, 60],
-                [460, 60, 460, 95],
-                [430, 95, 460, 95],
-                [430, 95, 430, 60],
-                [430, 60, 430, 25],
-                [430, 60, 460, 60],
-                [],  # dot
-            ],
-            [
-                [370, 25, 400, 25],
-                [400, 25, 400, 60],
-                [400, 60, 400, 95],
-                [370, 95, 400, 95],
-                [370, 95, 370, 60],
-                [370, 60, 370, 25],
-                [370, 60, 400, 60],
-                [],  # dot
-            ],
-            [
-                [310, 25, 340, 25],
-                [340, 25, 340, 60],
-                [340, 60, 340, 95],
-                [310, 95, 340, 95],
-                [310, 95, 310, 60],
-                [310, 60, 310, 25],
-                [310, 60, 340, 60],
-                [],  # dot
-            ],
-            [
-                [250, 25, 280, 25],
-                [280, 25, 280, 60],
-                [280, 60, 280, 95],
-                [250, 95, 280, 95],
-                [250, 95, 250, 60],
-                [250, 60, 250, 25],
-                [250, 60, 280, 60],
-                [],  # dot
-            ],
-            [
-                [190, 25, 220, 25],
-                [220, 25, 220, 60],
-                [220, 60, 220, 95],
-                [190, 95, 220, 95],
-                [190, 95, 190, 60],
-                [190, 60, 190, 25],
-                [190, 60, 220, 60],
-                [],  # dot
-            ],
-            [
-                [130, 25, 160, 25],
-                [160, 25, 160, 60],
-                [160, 60, 160, 95],
-                [130, 95, 160, 95],
-                [130, 95, 130, 60],
-                [130, 60, 130, 25],
-                [130, 60, 160, 60],
-                [],  # dot
-            ],
-            [
-                [70, 25, 100, 25],
-                [100, 25, 100, 60],
-                [100, 60, 100, 95],
-                [70, 95, 100, 95],
-                [70, 95, 70, 60],
-                [70, 60, 70, 25],
-                [70, 60, 100, 60],
-                [],  # dot
-            ],
-            [
-                # X,  Y,  x,  y
-                [10, 25, 40, 25],  # 0 line
-                [40, 25, 40, 60],  # 1 line
-                [40, 60, 40, 95],  # 2 ...
-                [10, 95, 40, 95],  # ...
-                [10, 95, 10, 60],
-                [10, 60, 10, 25],
-                [10, 60, 40, 60],
-                [],  # TODO: Add dots.
-            ],
-        ]
-
+        """
+        Draw symbol (activated lines) by byte representation on specified position.
+        """
         # Create new dict with lines from `lines` instead schematic numbers.
         new_bytes = {}
         for k, v in BYTES.items():
             new_bytes[hex(k)] = []
             for i in v:
-                new_bytes[hex(k)].append(self.lines[position][i])
+                new_bytes[hex(k)].append(LINES[position][i])
 
         # Required loop avoid bias connected lines.
-        for el in new_bytes[f"{byte}"]:
+        for el in new_bytes[hex(int(byte))]:
             self.canvas.create_line(el, fill=ctx.on_color)
 
         self.canvas.pack(fill=BOTH, expand=1)
 
 
-app = Tk()
-display = Display()
+display = Display(app)
+
+
+def set_method():
+    """
+    Function to handle methods from ZeroSeg library and save it to
+    list in `ctx` object.
+    """
+    while True:
+        from Emulator.library import METHOD
+
+        ctx.method = METHOD
+
+
+def ZeroSeg_code():
+    """
+    Testing function with ZeroSeg library example code.
+    """
+    from Emulator.library import screen
+    from datetime import datetime
+    import time
+
+    while True:
+        now = datetime.now()
+        screen.write_text(f"{now.hour}-{now.minute}-{now.second}")
+        time.sleep(1)
 
 
 def main():
-    from Emulator.library import screen
+    from threading import Thread
 
-    # Library methods.
-    screen.write_number(1)
-    screen.write_text("asdf")
-    screen.write_char("A", 3)
+    Thread(target=set_method, daemon=True).start()
+    Thread(target=ZeroSeg_code, daemon=True).start()
 
     app.geometry("400x250+300+300")
+    app.title(ctx.title)
     app.mainloop()
--- Emulator/library.py
+++ Emulator/library.py
@@ -4,26 +4,30 @@ Implementation of ZeroSeg library methods with corresponding
 behaviour but displayed on emulator display.
 """
 
-from Emulator.emulator import display
 from Emulator.characters import CHARS, UNDEFINED
+
+METHOD = []
 
 
 class Screen:
+    """
+    Representation of `screen` object from ZeroSeg library.
+    """
     def __init__(self):
         pass
 
     def set_byte(self, value: int, position: int, redraw: bool = True):
-        display.draw_symbol(str(hex(value)), position)
+        METHOD.append(f"draw_symbol(str(hex({value})), {position})")
         # TODO: redraw
 
-    def __get_byte_by_char(self, char: str):
+    def __get_byte_by_char(self, char: str) -> int:
         return hex(CHARS.get(str(char), UNDEFINED))
 
     def write_char(
         self, char: str, position: int = 1, dot: bool = False, redraw: bool = True
     ):
         value = self.__get_byte_by_char(char)
-        display.draw_symbol(str(value), position)
+        METHOD.append(f"draw_symbol({str(value)}, {position})")
         # TODO: redraw
 
     def write_number(
@@ -34,6 +38,7 @@ class Screen:
         zero_pad: bool = False,
         left_justify: bool = False,
     ):
+        self.clear()
         if zero_pad:
             pass
 
@@ -49,22 +54,26 @@ class Screen:
 
             for i in range(len(_bytes)):
                 if left_justify:
-                    display.draw_symbol(str(_bytes[i]), 8 - i)
+                    METHOD.append(f"draw_symbol(str({_bytes[i]}), 8 - {i})")
                 else:
-                    display.draw_symbol(str(_bytes[i]), i + 1)
+                    METHOD.append(f"draw_symbol(str({_bytes[i]}), {i} + 1)")
 
         # TODO: base
 
     def write_text(self, text: str):
+        self.clear()
         if len(text) <= 8:
             _bytes = []
             for i in text:
                 byte = str(self.__get_byte_by_char(i))
                 _bytes.append(byte)
             for i in range(len(text)):
-                display.draw_symbol(str(_bytes[i]), 8 - i)
+                METHOD.append(f"draw_symbol(str({_bytes[i]}), 8 - {i})")
         else:
             return
+
+    def clear(self):
+        METHOD.append("init_lines()")
 
 
 screen = Screen()
